39829a26c7dd9d676a3337ae9e7b5b0c7381da61
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2023-11-27 18:30:45 -0700

    added cards to show projects

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './works.css'
+import carShop from '../../assets/carShop.png'
+import toDoList from "../../assets/toDoList.png"
 
 
 const Works = () => {
@@ -7,10 +9,25 @@ const Works = () => {
     <section id="works">
         <h2 className="workTitle">My Portfolio</h2>
         <span className="workDesc">I am dedicated to delivering high-quality solutions that optimize complex workflows. As a Teaching Assistant, I've had the privilege of leading and mentoring students in web development, emphasizing usability, accessibility, and best practices. I am excited to bring my skills to the forefront, creating innovative and impactful digital experiences. Explore my portfolio and join me on this journey at the intersection of technology and creativity!</span>
+        
         <div className="workImgs">
-            <a href="https://cardeal.onrender.com"><div className='project1'><p>Backend of Dynamic car shop website</p></div></a>
-            <a href="https://todolist-237p.onrender.com"><div className='project1'><p>To Do list API with MongoDB and Swagger</p></div></a>
+            <div className='card'>
+          <a target='_blank' href="https://cardeal.onrender.com">
+              <img src={carShop} alt="" />
+              <h2>CarShop site</h2>
+              <p>Add cars for dynamic pages. Create account, log in; JWT ensures access, includes comprehensive error handling.</p>
+          </a>
+            </div>
+          
+            <div className='card'>
+          <a target='_blank' href="https://todolist-237p.onrender.com">
+              <img src={toDoList} alt="" />
+              <h2>To-Do List</h2>
+              <p>Express/MongoDB API with OAuth user authentication, account creation, Google login, and comprehensive error handling.</p>
+          </a>
+            </div>
         </div>
+
     </section>
   )
 }
