5d58efcb6315b561376c010d953b25e60958e459
Claudio Parra <101138305+cdp50@users.noreply.github.com>
2024-02-07 12:23:41 -0700

    intro improvement

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import './intro.css'
-import profileImg from '../../assets/profile2.png'
-import { Link } from 'react-scroll'
-import { IoMdBriefcase } from 'react-icons/io'
+import React from "react";
+import "./intro.css";
+import profileImg from "../../assets/profile2.png";
+import { Link } from "react-scroll";
+import { IoMdBriefcase } from "react-icons/io";
 
 const Intro = () => {
   return (
     <section id="intro">
-        <div className="introContent">
-            <span className="hello">Hello,</span>
-            <span className="introText">I'm 
-            <span className="introName"> Claudio</span>
-            <br/>Software Engineer</span>
-            <p className="introPara">I am a skilled Software Engineer with experience in React, and MongoDB <br /> </p>
-            <Link to='contact'><button className="btn"> <IoMdBriefcase/>Hire me</button></Link>
-        </div>
-        <img src={profileImg} alt=" profile image" className="bg" />
+      <div className="introContent">
+        <span className="hello">Hello, I'm</span>
+        <span className="introText">
+          <span className="introName"> Claudio</span>
+          <br />
+          Software Engineer
+        </span>
+        <p className="introPara">
+          I am a skilled <br /> Software <br /> Engineer with <br /> experience
+          in <br /> React, and <br />
+          MongoDB <br />{" "}
+        </p>
+        <Link to="contact">
+          <button className="btn">
+            {" "}
+            <IoMdBriefcase />
+            Hire me
+          </button>
+        </Link>
+      </div>
+      <img src={profileImg} alt=" profile image" className="bg" />
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
